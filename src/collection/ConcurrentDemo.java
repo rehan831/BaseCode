727dacc070889e87e623a838e3a84880879ae64e
Rehan Ansari <ansariand565@gmail.com>
2025-11-06 11:07:47 +0530

    core java

---
+++ src/collection/ConcurrentDemo.java
@@ -0,0 +1,18 @@
+package collection;
+import java.util.concurrent.*;
+public class ConcurrentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ConcurrentHashMap m = new ConcurrentHashMap();
+		m.put(101,"Rehan");
+		m.put(102,"durga");
+		m.putIfAbsent(103,"affan");
+		m.putIfAbsent(101,"Ansari");
+		m.remove(101,"durga");
+		m.replace(101,"Rehan","Rehaan");
+		System.out.println(m);
+	}
+
+}
